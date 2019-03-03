eef84107fdd5c01f3d8db487200a112e9623db14
Carol Chung <cch5ng@gmail.com>
2019-03-02 23:26:47 -0800

    refactor webpack config for dotenv

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,37 +1,39 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
-const env = dotenv.config().parsed;
-const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
+module.exports = () => {
+  const env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+      prev[`process.env.${next}`] = JSON.stringify(env[next]);
+      return prev;
+    }, {});
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-  	rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  resolve: {
-	 extensions: ['*', '.js', '.jsx']
-  },
-  output: {
-	   path: __dirname + '/dist',
-	   publicPath: '/',
-	   filename: 'bundle.js'
-  },
-  plugins: [
-	   new webpack.HotModuleReplacementPlugin(),
-     new webpack.DefinePlugin(envKeys)
-	],
-  devServer: {
-	   contentBase: './dist',
-	   hot: true
+  return {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+     extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+       path: __dirname + '/dist',
+       publicPath: '/',
+       filename: 'bundle.js'
+    },
+    plugins: [
+       new webpack.HotModuleReplacementPlugin(),
+       new webpack.DefinePlugin(envKeys)
+    ],
+    devServer: {
+       contentBase: './dist',
+       hot: true
+    }
   }
 };

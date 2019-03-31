41a979d05c8dc4ab9339ade9e391f9833f5ef764
Carol Chung <cch5ng@gmail.com>
2019-03-31 12:07:34 -0700

    stub out sections; add the iframe for youtube video

--- client/src/containers/stories/StoryDetail.js
+++ client/src/containers/stories/StoryDetail.js
@@ -21,32 +21,70 @@ class StoryDetail extends Component {
 		let idx;
 		let curStory;
 		let curVideo;
+		let videoSrc;
 		if (this.props && this.props.match) {
 			storyId = this.props.match.params.id;
 		}
 		if (this.props && this.props.idx) {
 			idx = this.props.idx;
-			console.log('idx', idx);
 		}
 		if (this.props && this.props.stories) {
 			curStory = this.props.stories.stories.filter(story => story.id.toString() === storyId)
+			curStory = curStory[0]
 			console.log('curStory', curStory)
+			videoSrc = `https://www.youtube.com/embed/${curStory.video.id.videoId}?autoplay=1`;
+			console.log('videoSrc', videoSrc)
 		}
 
-		return (
-			<div className={styles.storyDetail}>
-				<div className={styles.textContent}>
-					<p className={styles.category}>{curStory[0].category}</p>
-					<p className={styles.title}>{curStory[0].title}</p>
-					<p>{curStory[0].textShort}</p>
-				</div>
+/* placeholder
+
+		<div className={styles.buttonsContainer}></div>
+
 				<div className={styles.storyImage}>
 					<img src={curStory[0].video.snippet.thumbnails.high.url} alt={curStory[0].image.description} />
 					<p className={styles.imageCredit}>{curStory[0].author}</p>
 				</div>
+
+"https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+*/
+
+		return (
+			<div className={styles.storyDetail}>
+				<div className={styles.textContent}>
+					<p className={styles.category}>{curStory.category}</p>
+					<p className={styles.titleDetail}>{curStory.title}</p>
+					<p>{curStory.textShort}</p>
+				</div>
+				<div className={styles.storyMetaContainer}>
+					<div>date time</div>
+					<div>show name</div>
+					<div>reporter container
+						<div>reporter headshot</div>
+						<div>reporter byline</div>
+						<div>reporter social icons container
+							<div>facebook link</div>
+							<div>twitter link</div>
+							<div>instagram link</div>
+						</div>
+					</div>
+				</div>
+
+				<div className={styles.ytContainer}>
+					<iframe id="ytplayer" type="text/html" className={styles.respIframe}
+					  src={videoSrc}
+					  frameBorder="0"></iframe>
+				</div>
+
+				<div>transcript</div>
+				<div>story social sharing icons container</div>
+				<div>
+					<div>newsletter signup box heading</div>
+					<div>newsletter signup text</div>
+					<div>email input</div>
+					<button>SUBSCRIBE</button>
+				</div>
 			</div>
 		)
-
 	}
 }
 

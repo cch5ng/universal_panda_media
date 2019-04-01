28baae63c90afcd9859918343d55d228b0d0d8af
Carol Chung <carol.chung@submittable.com>
2019-04-01 12:23:00 -0700

    2b header footer (#21)
    * cleanup, remove some unused http request definitions
    
    * add font for logo
    
    * add header content
    
    * basic header with nav; right now non differentiated between mobile and desktop but looks OK for moment
    
    * update footer content and styling

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import classNames from 'classnames/bind';
 import Stories from '../containers/stories/Stories';
 import ScrollToTop from './ScrollToTop';
 import { fetchStories } from '../containers/stories/StoryActions';
 import StoryDetail from './stories/StoryDetail';
-//import styles from './App.css';
+import styles from './App.css';
+
+let cx = classNames.bind(styles);
 
 class App extends Component {
 
@@ -13,20 +16,48 @@ class App extends Component {
     super(props);
 
     this.state = {
+      displayNavMenu: false,
     }
 
-    //this.startCountdownTimer = this.startCountdownTimer.bind(this);
+    this.toggleNavigationMenu = this.toggleNavigationMenu.bind(this);
+  }
+
+  toggleNavigationMenu(ev) {
+    ev.preventDefault();
+    this.setState((state, props) => {
+      return { displayNavMenu: !state.displayNavMenu }
+    });
+
   }
 
   render() {
+    let navCloseButtonClass = cx({
+      navMenuItem: true,
+      closeButton: true
+    });
 
+    //let hide = {styles.hide}
   	return (
-
   		<Router>
         <ScrollToTop>
     			<div className="App">
     				<header>
-    					<nav></nav>
+    					<nav>
+                <div className={styles.logo}>UPV</div>
+                <div className={this.state.displayNavMenu ? styles.hide : styles.navButtonContainer}>
+                  <button className={styles.navButton} onClick={this.toggleNavigationMenu}>
+                    &#9776;
+                  </button>
+                </div>
+                <div className={this.state.displayNavMenu ? styles.navMenuContainer :  styles.hide}>
+                  <div className={navCloseButtonClass} onClick={this.toggleNavigationMenu}>Close X</div>
+                  <div className={styles.navMenuItem}>News</div>
+                  <div className={styles.navMenuItem}>Arts & Life</div>
+                  <div className={styles.navMenuItem}>Search</div>
+                  <div className={styles.navMenuItem}>Panda Store</div>
+
+                </div>
+              </nav>
     				</header>
     				<main>
     					<Switch>
@@ -38,7 +69,45 @@ class App extends Component {
                   )}/>
     					</Switch>
     				</main>
-    				<footer></footer>
+    				<footer>
+              <div className={styles.footerLinksContainer}>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>READ & WATCH</div>
+                  <div className={styles.footerLink}>Home</div>
+                  <div className={styles.footerLink}>News</div>
+                  <div className={styles.footerLink}>Arts & Life</div>
+                  <div className={styles.footerLink}>Programs</div>
+                </div>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>CONNECT</div>
+                  <div className={styles.footerLink}>Newsletters</div>
+                  <div className={styles.footerLink}>Facebook</div>
+                  <div className={styles.footerLink}>Twitter</div>
+                  <div className={styles.footerLink}>Contact</div>
+                </div>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>ABOUT UPV</div>
+                  <div className={styles.footerLink}>Overview</div>
+                  <div className={styles.footerLink}>Finances</div>
+                  <div className={styles.footerLink}>People</div>
+                  <div className={styles.footerLink}>Press</div>
+                </div>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>GET INVOLVED</div>
+                  <div className={styles.footerLink}>Support UPV</div>
+                  <div className={styles.footerLink}>UPV Careers</div>
+                  <div className={styles.footerLink}>UPV Shop</div>
+                  <div className={styles.footerLink}>Visit UPV</div>
+                </div>
+              </div>
+              <div className={styles.footerTermsContainer}>
+                <div className={styles.footerTermsLinkItem}>terms of use</div>
+                <div className={styles.footerTermsLinkItem}>privacy</div>
+                <div className={styles.footerTermsLinkItem}>your privacy choices</div>
+                <div className={styles.footerTermsLinkItem}>text only</div>
+                <div className={styles.footerTermsItem}>2019 UPV</div>
+              </div>
+            </footer>
     			</div>
         </ScrollToTop>
   		</Router>
--- client/src/utils/http_requests.js
+++ client/src/utils/http_requests.js
@@ -1,9 +1,6 @@
 const JSON_SERVER_API_ROOT = process.env.JSON_SERVER_API_ROOT;
 const STORIES_ALL = 'stories';
 
-const UNSPLASH_API_ROOT = 'https://api.unsplash.com/';
-const IMAGES_NATURE = 'search/photos?query=nature&per_page=30';
-
 const YOUTUBE_API_ROOT = 'https://content.googleapis.com/youtube/v3/';
 const YOUTUBE_VIDEOS_REQUEST = `search?part=snippet&maxResults=25&q=doxiemom19%20panda&key=${process.env.YOUTUBE_CLIENT_KEY}`;
 const YOUTUBE_VIDEO_BY_ID_REQUEST_MID = `videos?part=snippet%2CcontentDetails%2Cstatistics&id=`
@@ -15,17 +12,6 @@ const requests = {
       fetch(url)
       	.then(resp => resolve(resp.json()))
 	      .catch(err => reject(err))
-    })
-  },
-  getImages: url => {
-    return new Promise((resolve, reject) => {
-      fetch(url,
-        {headers: {
-          "Authorization": process.env.UNSPLASH_CLIENT_KEY
-        }}
-      )
-        .then(resp => resolve(resp.json()))
-        .catch(err => reject(err))
     })
   },
   getVideoById: url => {
@@ -43,12 +29,6 @@ const Stories = {
   },
 }
 
-const Images = {
-  getAllImages: () => {
-    return requests.getImages(`${UNSPLASH_API_ROOT}${IMAGES_NATURE}`)
-  },
-}
-
 const Videos = {
   getAll: () => {
     return requests.get(`${YOUTUBE_API_ROOT}${YOUTUBE_VIDEOS_REQUEST}`)
@@ -63,6 +43,5 @@ const Videos = {
 
 export default {
   Stories,
-  Images,
   Videos,
 }

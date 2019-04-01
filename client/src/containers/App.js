ac846e15264971c2817a5518f91cad03cd0fad3c
Carol Chung <cch5ng@gmail.com>
2019-04-01 12:30:46 -0700

    update footer content and styling

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -69,7 +69,45 @@ class App extends Component {
                   )}/>
     					</Switch>
     				</main>
-    				<footer></footer>
+    				<footer>
+              <div className={styles.footerLinksContainer}>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>READ & WATCH</div>
+                  <div className={styles.footerLink}>Home</div>
+                  <div className={styles.footerLink}>News</div>
+                  <div className={styles.footerLink}>Arts & Life</div>
+                  <div className={styles.footerLink}>Programs</div>
+                </div>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>CONNECT</div>
+                  <div className={styles.footerLink}>Newsletters</div>
+                  <div className={styles.footerLink}>Facebook</div>
+                  <div className={styles.footerLink}>Twitter</div>
+                  <div className={styles.footerLink}>Contact</div>
+                </div>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>ABOUT UPV</div>
+                  <div className={styles.footerLink}>Overview</div>
+                  <div className={styles.footerLink}>Finances</div>
+                  <div className={styles.footerLink}>People</div>
+                  <div className={styles.footerLink}>Press</div>
+                </div>
+                <div className={styles.footerLinksSection}>
+                  <div className={styles.footerLinkHeading}>GET INVOLVED</div>
+                  <div className={styles.footerLink}>Support UPV</div>
+                  <div className={styles.footerLink}>UPV Careers</div>
+                  <div className={styles.footerLink}>UPV Shop</div>
+                  <div className={styles.footerLink}>Visit UPV</div>
+                </div>
+              </div>
+              <div className={styles.footerTermsContainer}>
+                <div className={styles.footerTermsLinkItem}>terms of use</div>
+                <div className={styles.footerTermsLinkItem}>privacy</div>
+                <div className={styles.footerTermsLinkItem}>your privacy choices</div>
+                <div className={styles.footerTermsLinkItem}>text only</div>
+                <div className={styles.footerTermsItem}>2019 UPV</div>
+              </div>
+            </footer>
     			</div>
         </ScrollToTop>
   		</Router>

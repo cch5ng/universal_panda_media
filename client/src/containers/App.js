b5e91ba654a4400d225fd3a9b55b04d37fcb3025
Carol Chung <cch5ng@gmail.com>
2019-04-01 11:01:02 -0700

    basic header with nav; right now non differentiated between mobile and desktop but looks OK for moment

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import classNames from 'classnames/bind';
 import Stories from '../containers/stories/Stories';
 import ScrollToTop from './ScrollToTop';
 import { fetchStories } from '../containers/stories/StoryActions';
 import StoryDetail from './stories/StoryDetail';
 import styles from './App.css';
+
+let cx = classNames.bind(styles);
 
 class App extends Component {
 
@@ -13,22 +16,47 @@ class App extends Component {
     super(props);
 
     this.state = {
+      displayNavMenu: false,
     }
 
-    //this.startCountdownTimer = this.startCountdownTimer.bind(this);
+    this.toggleNavigationMenu = this.toggleNavigationMenu.bind(this);
+  }
+
+  toggleNavigationMenu(ev) {
+    ev.preventDefault();
+    this.setState((state, props) => {
+      return { displayNavMenu: !state.displayNavMenu }
+    });
+
   }
 
   render() {
+    let navCloseButtonClass = cx({
+      navMenuItem: true,
+      closeButton: true
+    });
 
+    //let hide = {styles.hide}
   	return (
-
   		<Router>
         <ScrollToTop>
     			<div className="App">
     				<header>
     					<nav>
                 <div className={styles.logo}>UPV</div>
-                <div ><button className={styles.navButton}>&#9776;</button></div>
+                <div className={this.state.displayNavMenu ? styles.hide : styles.navButtonContainer}>
+                  <button className={styles.navButton} onClick={this.toggleNavigationMenu}>
+                    &#9776;
+                  </button>
+                </div>
+                <div className={this.state.displayNavMenu ? styles.navMenuContainer :  styles.hide}>
+                  <div className={navCloseButtonClass} onClick={this.toggleNavigationMenu}>Close X</div>
+                  <div className={styles.navMenuItem}>News</div>
+                  <div className={styles.navMenuItem}>Arts & Life</div>
+                  <div className={styles.navMenuItem}>Search</div>
+                  <div className={styles.navMenuItem}>Panda Store</div>
+
+                </div>
               </nav>
     				</header>
     				<main>
